2b9961bf72e24beac97634c78b3e1b88571ce0b5
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-21 19:58:19 +0530

    AI: create src/main/java/com/mephistopheles/vulkanboost/VulkanBoostMod.java

---
+++ src/main/java/com/mephistopheles/vulkanboost/VulkanBoostMod.java
@@ -0,0 +1,10 @@
+package com.mephistopheles.vulkanboost;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VulkanBoostMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("VulkanBoost Initialized!");
+    }
+}
