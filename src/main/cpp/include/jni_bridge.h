2f81023d7bf0c04ea9453e521175ba110172ad03
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-21 20:10:19 +0530

    Update jni_bridge.h

--- src/main/cpp/include/jni_bridge.h
+++ src/main/cpp/include/jni_bridge.h
@@ -1,10 +1,23 @@
-#include <jni.h>
-
 #ifndef JNI_BRIDGE_H
 #define JNI_BRIDGE_H
 
+#include <jni.h>
+
+// JNI function declarations for VulkanRenderer
 extern "C" {
-    JNIEXPORT void JNICALL Java_com_mephistopheles_vulkanboost_vulkan_VulkanRenderer_initVulkan(JNIEnv *, jobject);
+
+JNIEXPORT jboolean JNICALL
+Java_com_mephistopheles_vulkanboost_vulkan_VulkanRenderer_isVulkanSupported(JNIEnv *env, jclass clazz);
+
+JNIEXPORT void JNICALL
+Java_com_mephistopheles_vulkanboost_vulkan_VulkanRenderer_init(JNIEnv *env, jclass clazz);
+
+JNIEXPORT void JNICALL
+Java_com_mephistopheles_vulkanboost_vulkan_VulkanRenderer_renderFrame(JNIEnv *env, jclass clazz);
+
+JNIEXPORT void JNICALL
+Java_com_mephistopheles_vulkanboost_vulkan_VulkanRenderer_cleanup(JNIEnv *env, jclass clazz);
+
 }
 
-#endif
+#endif // JNI_BRIDGE_H
