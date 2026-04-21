acbc6d07b4ed2e4f8badf29ae4baa316a42e8324
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-21 20:06:50 +0530

    Update vulkan_renderer.cpp

--- src/main/cpp/src/vulkan_renderer.cpp
+++ src/main/cpp/src/vulkan_renderer.cpp
@@ -1,6 +1,63 @@
-#include "vulkan_renderer.h"
+#include <vulkan/vulkan.h>
 #include <iostream>
+#include <vector>
 
-void VulkanRenderer::init() {
-    std::cout << "Vulkan Initialized Native" << std::endl;
+VkInstance instance;
+VkPhysicalDevice physicalDevice;
+VkDevice device;
+VkQueue graphicsQueue;
+
+bool isVulkanSupported() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    return layerCount > 0; // Basic check
+}
+
+void initVulkan() {
+    // Vulkan Instance
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "VulkanBoost";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create Vulkan instance!");
+    }
+
+    // Pick GPU
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0) {
+        throw std::runtime_error("No Vulkan-compatible GPU!");
+    }
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+    physicalDevice = devices[0]; // Use first GPU
+
+    // Create Logical Device
+    VkDeviceCreateInfo deviceCreateInfo = {};
+    deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+    if (vkCreateDevice(physicalDevice, &deviceCreateInfo, nullptr, &device) != VK_SUCCESS) {
+        throw std::runtime_error("Failed to create logical device!");
+    }
+
+    // Get Graphics Queue
+    vkGetDeviceQueue(device, 0, 0, &graphicsQueue);
+}
+
+void renderFrameVulkan() {
+    // TODO: Implement Vulkan frame rendering
+    // (Use command buffers, swapchain, etc.)
+}
+
+void cleanupVulkan() {
+    vkDestroyDevice(device, nullptr);
+    vkDestroyInstance(instance, nullptr);
 }
