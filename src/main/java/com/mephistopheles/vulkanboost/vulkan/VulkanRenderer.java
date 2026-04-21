d01d686dfbed1d5237d0cf26cd8c602f2832774f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-21 20:04:19 +0530

    Update VulkanRenderer.java

--- src/main/java/com/mephistopheles/vulkanboost/vulkan/VulkanRenderer.java
+++ src/main/java/com/mephistopheles/vulkanboost/vulkan/VulkanRenderer.java
@@ -1,7 +1,15 @@
 package com.mephistopheles.vulkanboost.vulkan;
 
 public class VulkanRenderer {
-    public native void initVulkan();
-    public native void render();
-    public native void cleanup();
+    static {
+        NativeLibraryLoader.loadLibrary();
+    }
+
+    // Check if Vulkan is supported
+    public static native boolean isVulkanSupported();
+
+    // Native Methods (C++ Implementation)
+    public static native void init();
+    public static native void renderFrame();
+    public static native void cleanup();
 }
