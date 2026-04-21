c598403b8a535a3d12c6aa252910ff1c23d54a59
twiceextra57863-source <twiceextra57863@gmail.com>
2026-04-21 20:10:49 +0530

    Update vulkan_renderer.h

--- src/main/cpp/include/vulkan_renderer.h
+++ src/main/cpp/include/vulkan_renderer.h
@@ -1,11 +1,25 @@
 #ifndef VULKAN_RENDERER_H
 #define VULKAN_RENDERER_H
 
-class VulkanRenderer {
-public:
-    void init();
-    void render();
-    void cleanup();
-};
+#include <vulkan/vulkan.h>
+#include <vector>
+#include <stdexcept>
 
-#endif
+// Vulkan core objects
+extern VkInstance instance;
+extern VkPhysicalDevice physicalDevice;
+extern VkDevice device;
+extern VkQueue graphicsQueue;
+
+// Functions
+bool isVulkanSupported();
+void initVulkan();
+void renderFrameVulkan();
+void cleanupVulkan();
+
+// Helper functions
+VkPhysicalDevice pickPhysicalDevice(VkInstance instance);
+uint32_t findGraphicsQueueFamily(VkPhysicalDevice device);
+void createLogicalDevice(VkPhysicalDevice physicalDevice, VkDevice* device, VkQueue* graphicsQueue);
+
+#endif // VULKAN_RENDERER_H
